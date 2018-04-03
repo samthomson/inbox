862275d210a76e4ef471ab3bc2b9059d07d36196
Sam Thomson <samt@samt.st>
2018-04-03 21:34:36 +0100

    refactor, export compile outcome

--- compile.js
+++ compile.js
@@ -5,5 +5,4 @@ const solc = require('solc')
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol')
 const source = fs.readFileSync(inboxPath, 'utf8')
 
-const compiled = solc.compile(source, 1)
-console.log(compiled)
+modfule.exports = solc.compile(source, 1).contracts[':Inbox']
