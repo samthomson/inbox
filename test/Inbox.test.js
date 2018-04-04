0ded49b70fc60fccba48b56c5d1eb375df627b8a
Sam Thomson <samt@samt.st>
2018-04-04 19:39:03 +0100

    testing++

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,7 +1,8 @@
 const assert = require('assert')
 const ganache = require('ganache-cli')
 const Web3 = require('web3')
-const web3 = new Web3(ganache.provider())
+const provider = ganache.provider()
+const web3 = new Web3(provider)
 const { interface, bytecode } = require('../compile')
 
 let accounts
@@ -23,10 +24,12 @@ beforeEach(async () => {
             from: accounts[0],
             gas: '1000000'
         })
+    
+    inbox.setProvider(provider)
 })
 
 describe('Inbox contract', () => {
     it('deploys a contract', () => {
-        console.log(inbox)
+        assert.ok(inbox.options.address)
     })
 })
