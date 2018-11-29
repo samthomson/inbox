bb6cb1409ff2f641ac75a07294e78863e0dbe4fc
Sam Thomson <samt@samt.st>
2018-11-29 19:32:35 +0700

    tidier config

--- deploy.js
+++ deploy.js
@@ -1,11 +1,11 @@
 const HDWalletProvider = require('truffle-hdwallet-provider')
 const Web3 = require('web3')
 const { interface, bytecode } = require('./compile')
-const env = require('./env')
+const envVars = require('./env')
 
 const provider = new HDWalletProvider(
-    env.mneumonic,
-    'https://rinkeby.infura.io/v3/104f11b7052c460195dfeecfc4e4260b'
+    envVars.mneumonic,
+    envVars.deployUrl
 )
 
 const web3 = new Web3(provider)
